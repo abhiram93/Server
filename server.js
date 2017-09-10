9dda0dfe3d737355416943634e0990b65406f2c4
vegetau <uppoorabhiram@outlook.com>
2017-09-11 01:26:14 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const hbs = require('hbs');
+
+var app = express();
+
+
+hbs.registerPartials(__dirname+'/views/partials');
+app.set('view engine','hbs');
+app.use(express.static(__dirname+'/public'));
+
+
+
+app.use(function(req,res,next) {
+   var now = new Date().toString();
+    console.log(now);
+    console.log(req.method);
+    console.log(req.url);
+    
+    next(); 
+});
+
+app.use(function(re,res,next) {
+    res.render('maintenance.hbs');
+})
+
+app.get('/',function(req,res) {
+    //res.send('<h1>Hello express</h1>');
+    /*res.send( {
+        name:'Abhi',
+        likes:[
+            'Games',
+            'Youtube'
+        ]
+    });*/
+    res.render('home.hbs', {
+        name:'Abhi',
+        currentYear:new Date().getFullYear(),
+        message:'Hello. Hi'
+    });
+});
+
+app.get('/about',function(req,res) {
+    res.render('about.hbs', {
+        pageTitle:'About Page',
+        currentYear: new Date().getFullYear()
+    });
+});
+
+app.get('/bad',function(req,res) {
+    res.send({
+        errorMessage:'Unable to handle request' 
+    });
+})
+
+app.listen(3000, function() {
+    console.log('Server Started');
+});
